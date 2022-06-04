50db1951ffb4c191b40e1a0c725de6cb5fefbc72
hanglong4000 <sapbatcheung@gmail.com>
2022-06-04 23:05:30 +0700

    good

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,15 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  action: {},
+  state: {
+    //   change the state here
+  },
+  mutations: {
+    //   The mutation is here
+  },
+  action: {
+    //   take the action
+  },
   modules: {},
   getters: {},
-  setters: {},
 });
