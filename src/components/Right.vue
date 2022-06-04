3450e60913d9f83a1382291ac36a2d5733eb001c
hanglong4000 <sapbatcheung@gmail.com>
2022-06-04 18:33:16 +0700

    good

--- src/components/Right.vue
+++ src/components/Right.vue
@@ -1,24 +1,29 @@
-<template><h3>{{settings.content}}</h3></template>
+<template>
+  <h3>{{ settings.content }}</h3>
+</template>
 <script>
 export default {
-    computed: {
-        settings() {
-            return [{
-                name: 'content',
-                type: 'textarea',
-                value: 'Enter your heading text'
-            }, {
-                name: 'tag',
-                type: 'select',
-                value: 'h3',
-                options: {
-                    'h1': 'H1',
-                    'h2': 'H2',
-                    'h3': 'H3',
-                    'h4': 'H4',
-                }
-            }, ]
-        }
-    }
-}
+  computed: {
+    settings() {
+      return [
+        {
+          name: "content",
+          type: "textarea",
+          value: "Enter your heading text",
+        },
+        {
+          name: "tag",
+          type: "select",
+          value: "h3",
+          options: {
+            h1: "H1",
+            h2: "H2",
+            h3: "H3",
+            h4: "H4",
+          },
+        },
+      ];
+    },
+  },
+};
 </script>
