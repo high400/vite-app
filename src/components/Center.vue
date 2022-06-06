428b0087e916f998bac7aac05d70d43f4af4b0d8
hanglong4000 <sapbatcheung@gmail.com>
2022-06-06 08:11:51 +0700

    drag and drop

--- src/components/Center.vue
+++ src/components/Center.vue
@@ -1,15 +1,15 @@
 <template>
-<div class="middle h-screen">
+  <div class="middle h-screen">
     <img class="pic" src="../assets/Quang_Trung.png" />
-</div>
+  </div>
 </template>
 <script>
-    const receiver = [
-        
-    ];
+const receiver = [];
 </script>
 <style>
 .middle {
-    background-color: white;
+  background-color: white;
+  margin-bottom: 10px;
+  padding: 10px;
 }
 </style>
--- src/components/Right.vue
+++ src/components/Right.vue
@@ -1,16 +1,56 @@
 <template>
   <h3>{{ settings.content }}</h3>
   <div class="flex justify-around">
-    <div title="Content" class="border-1 border-solid border-black">Content</div>
-    <div title="Design" class="border-1 border-solid border-black">Design</div>
-    <div><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-</svg></div>
+    <div title="Content" class="border-1 border-solid border-black">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+        /></svg
+      >Content
+    </div>
+    <div title="Design" class="border-1 border-solid border-black">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      Design
+    </div>
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+        />
+      </svg>
+    </div>
   </div>
 </template>
 <script>
-// import TabWrapper from "./TabWrapper.vue";
-// import Tabs from "./Tabs.vue";
 export default {
   computed: {
     settings() {
